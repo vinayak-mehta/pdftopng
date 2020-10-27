b6edd49a927df539db2785688ccfe2150c587828
Vinayak Mehta <vmehta94@gmail.com>
2020-10-27 05:54:24 +0530

    Add cmake generator platform

--- scripts/build_win_x64.bat
+++ scripts/build_win_x64.bat
@@ -4,6 +4,7 @@ vcpkg install freetype:x64-windows fontconfig:x64-windows libpng:x64-windows lib
 Rem set PATH=%PATH%;.\vcpkg\installed\x64-windows\bin
 
 cd lib\poppler
-mkdir build && cd build
-cmake -DCMAKE_TOOLCHAIN_FILE=%VCPKG_INSTALLATION_ROOT%/scripts/buildsystems/vcpkg.cmake -DENABLE_QT5=OFF -DENABLE_LIBOPENJPEG=none -DENABLE_CPP=OFF ..
+mkdir build_x64 && cd build_x64
+cmake -A x64 -DCMAKE_TOOLCHAIN_FILE=%VCPKG_INSTALLATION_ROOT%/scripts/buildsystems/vcpkg.cmake -DENABLE_QT5=OFF -DENABLE_LIBOPENJPEG=none -DENABLE_CPP=OFF ..
 cmake --build . --config Release --target poppler
+cd ..\..\..
--- scripts/build_win_x86.bat
+++ scripts/build_win_x86.bat
@@ -4,6 +4,7 @@ vcpkg install freetype:x86-windows fontconfig:x86-windows libpng:x86-windows lib
 Rem set PATH=%PATH%;.\vcpkg\installed\x86-windows\bin
 
 cd lib\poppler
-mkdir build && cd build
-cmake -DCMAKE_TOOLCHAIN_FILE=%VCPKG_INSTALLATION_ROOT%/scripts/buildsystems/vcpkg.cmake -DENABLE_QT5=OFF -DENABLE_LIBOPENJPEG=none -DENABLE_CPP=OFF ..
+mkdir build_x86 && cd build_x86
+cmake -A Win32 -DCMAKE_TOOLCHAIN_FILE=%VCPKG_INSTALLATION_ROOT%/scripts/buildsystems/vcpkg.cmake -DENABLE_QT5=OFF -DENABLE_LIBOPENJPEG=none -DENABLE_CPP=OFF ..
 cmake --build . --config Release --target poppler
+cd ..\..\..
