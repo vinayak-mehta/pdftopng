87cab00613ab51269de11f193c861d909e67da56
Vinayak Mehta <vmehta94@gmail.com>
2021-04-05 04:12:06 +0530

    Add poppler build step

---
+++ scripts/build_ubuntu.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+apt install cmake libfreetype-dev libfontconfig-dev libpng-dev libjpeg-dev
+
+cd lib/poppler
+mkdir build && cd build
+cmake -D ENABLE_QT5=OFF -D ENABLE_LIBOPENJPEG=none -D ENABLE_CPP=OFF ..
+make poppler
