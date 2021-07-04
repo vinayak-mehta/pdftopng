41fa1ef64b3cf2f8cc4b1b6ebad467b1b480b6e0
Vinayak Mehta <vmehta94@gmail.com>
2021-07-05 03:40:11 +0530

    Find cmake latest framework in macOS build

--- scripts/build_macos.sh
+++ scripts/build_macos.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-brew install freetype fontconfig libpng jpeg
+brew install pkg-config freetype fontconfig libpng jpeg
 
 cd lib/poppler
 mkdir build && cd build
-cmake -D ENABLE_QT5=OFF -D ENABLE_LIBOPENJPEG=none -D ENABLE_CPP=OFF -D ENABLE_BOOST=OFF ..
+cmake -D CMAKE_FIND_FRAMEWORK=LAST -D ENABLE_QT5=OFF -D ENABLE_LIBOPENJPEG=none -D ENABLE_CPP=OFF -D ENABLE_BOOST=OFF ..
 make poppler
