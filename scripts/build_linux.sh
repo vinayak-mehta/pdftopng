1c14c7fc287743525c09bb765f63c8a9da87a20d
Vinayak Mehta <vmehta94@gmail.com>
2021-07-05 00:07:24 +0530

    Fix build_linux.sh

--- scripts/build_linux.sh
+++ scripts/build_linux.sh
@@ -2,11 +2,11 @@
 
 yum install -y wget freetype-devel fontconfig-devel libpng-devel libjpeg-devel
 
-wget https://cmake.org/files/v3.10/cmake-3.10.2.tar.gz
-tar zxvf cmake-3.10.2.tar.gz
+wget https://cmake.org/files/v3.10/cmake-3.10.2-Linux-x86_64.tar.gz
+tar zxvf cmake-3.10.2-Linux-x86_64.tar.gz
 current_directory=$(pwd)
 
 cd lib/poppler
 mkdir build && cd build
-$(current_directory)/cmake-3.10.2-Linux-x86_64/bin/cmake -D ENABLE_QT5=OFF -D ENABLE_LIBOPENJPEG=none -D ENABLE_CPP=OFF -D ENABLE_BOOST=OFF ..
+"$current_directory"/cmake-3.10.2-Linux-x86_64/bin/cmake -D ENABLE_QT5=OFF -D ENABLE_LIBOPENJPEG=none -D ENABLE_CPP=OFF -D ENABLE_BOOST=OFF ..
 make poppler
